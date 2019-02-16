8f4f28bfe1c4eddbc45f12971c53d5da96a1294e
mazenmel <45897136+mazenmel@users.noreply.github.com>
2019-02-16 20:33:33 +0100

    Create HomographyNet.py

---
+++ HomographyNet.py
@@ -0,0 +1,60 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model,self).__init__()
+        self.layer1 = nn.Sequential(nn.Conv2d(2,64,3,padding=1),
+                                    nn.BatchNorm2d(64),
+                                    nn.ReLU())
+                                    
+        self.layer2 = nn.Sequential(nn.Conv2d(64,64,3,padding=1),
+                                    nn.BatchNorm2d(64),
+                                    nn.ReLU(),
+                                    nn.MaxPool2d(2))
+        self.layer3 = nn.Sequential(nn.Conv2d(64,64,3,padding=1),
+                                    nn.BatchNorm2d(64),
+                                    nn.ReLU())
+        self.layer4 = nn.Sequential(nn.Conv2d(64,64,3,padding=1),
+                                    nn.BatchNorm2d(64),
+                                    nn.ReLU(),
+                                    nn.MaxPool2d(2))
+        self.layer5 = nn.Sequential(nn.Conv2d(64,128,3,padding=1),
+                                    nn.BatchNorm2d(128),
+                                    nn.ReLU())        
+        self.layer6 = nn.Sequential(nn.Conv2d(128,128,3,padding=1),
+                                    nn.BatchNorm2d(128),
+                                    nn.ReLU(),
+                                    nn.MaxPool2d(2))
+        self.layer7 = nn.Sequential(nn.Conv2d(128,128,3,padding=1),
+                                    nn.BatchNorm2d(128),
+                                    nn.ReLU())
+        self.layer8 = nn.Sequential(nn.Conv2d(128,128,3,padding=1),
+                                    nn.BatchNorm2d(128),
+                                    nn.ReLU())
+        self.fc1 = nn.Linear(128*16*16,1024)
+        self.fc2 = nn.Linear(1024,8)
+        
+    def forward(self,x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = out.view(-1,128* 16* 16)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+        
+from torchsummary import summary 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+model = Model().to(device)
+
+summary(model,(2,128,128))
+
+import torch.optim as optim
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(),lr=0.005, momentum=0.9)
